4670336bda14e85f707e0a321ec35176625b6dee
diegoflores93 <diegoa.flores@outlook.com>
2017-06-12 22:29:36 -0500

    Initial commit

---
+++ app/src/main/java/com/diegoflores/finalpuppycoursera/MainActivity.java
@@ -0,0 +1,13 @@
+package com.diegoflores.finalpuppycoursera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
